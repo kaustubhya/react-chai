52e5b884e166ece97864c74ede8a66112c6e4aea
Kaustubhya Shukla <kaustubhyaksd@gmail.com>
2024-01-03 05:22:58 +0530

    counters project_useState done

---
+++ react-03-hhoks-counter-project/counter app hooks/notes.md
@@ -0,0 +1,6 @@
+In backend JS plays the role in updating things, but to display and update changes in UI, React comes into play by using 'HOOKS'.
+
+• useState hook is used to change the state (not update the values). This hook propagates the changes to the UI (DOM).
+
+We get 2 things as output from the useState hook. These are in the form of an array.
+[counter, setCounter (this is a function that updates counter)]
--- react-03-hhoks-counter-project/counter app hooks/src/App.jsx
+++ react-03-hhoks-counter-project/counter app hooks/src/App.jsx
@@ -1,14 +1,50 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  // let counter = 5;
+  let value = 0;
+  let [counter, setCounter] = useState(value);
+  // 15 is the default value
+  const addValue = () => {
+    // console.log("Clicked", Math.random());
+    
+    // counter += 1;
+    // setCounter[counter];
+    // setCounter now has the updated counter value
+    //  or we can de 
+    if(counter < 20) {
+    console.log("Clicked", counter);
+    setCounter(counter + 1);
+    }
+
+  }
+
+  const removeValue = () => {
+    // console.log("Clicked", Math.random());
+    
+    if(counter > 0) {
+      console.log("Clicked", counter);
+      setCounter(counter - 1);
+      }
+    
+    // counter += 1;
+    // setCounter[counter];
+    // setCounter now has the updated counter value
+    //  or we can de 
+
+  }
 
   return (
     <>
     <h1>Jai MATAA DI</h1>
-    <h3>Counter Value: 11</h3>
+    <h3>Counter Value: {counter}</h3>
+
+    <button
+    onClick={addValue}>Add Value{counter}</button>
+    <br />
+    <button onClick={removeValue}>Remove Value {counter}</button>
+    <p>footer: {counter}</p>
     </>
   )
 }
