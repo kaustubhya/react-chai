f47cecf6b7c4f016ec4d16f4013a558bafd475f6
Kaustubhya Shukla <kaustubhyaksd@gmail.com>
2024-01-29 20:00:53 +0530

    here

--- react-12-redux-toolkit-todo/src/app/store.js
+++ react-12-redux-toolkit-todo/src/app/store.js
@@ -4,7 +4,18 @@
 import {configureStore} from '@reduxjs/toolkit';
 // 2. Store is now done but it is not ready yet for use-case, so using this method, let us export a variable.
 
-export const store = configureStore({})
+// **************** importing reducers list
+import todoReducer from "./features/todo/todoSlice";
+// *************** Now add the following keys in the configure store object 
+
+export const store = configureStore({
+    reducer: todoReducer
+
+    // 5. Go to components, addtodo.jsx and todo.jsx and do rfce on both
+})
 
 // Now we will make reducers
 // 3. Go to src/features/todo/todoSlice.js 
+
+
+// ******************** After coming back from todoSlice.js, we first import all reducers list
--- react-12-redux-toolkit-todo/src/features/todo/todoSlice.js
+++ react-12-redux-toolkit-todo/src/features/todo/todoSlice.js
@@ -41,18 +41,67 @@ export const todoSlice = createSlice({
         // We can declare functions either here or at some other place 
         // addTodo: sayHello (Method 1)
 
-        addTodo: () => {},
         // Method 2
-        removeTodo: () => {}
-        
+        addTodo: (state, action) => {
         // Now in context API, we only declared the function and not initialize and write it
         // Here we will initialize as well as write it
 
         // 🛑🛑🛑🛑 Now just like useState and useEffect we get 2 things inside addTodo function (state, action), we will have access to both here always
         // State here gives us access to all the default values present already inside the initialState
         // As for action, we get those values from it which we want to use in a funciton, say to remove a todo, a need an ID, this ID, we will get it from action.
+        // Jo data pass ho raha hai, wo milta hai action mein
+
+        // Now we will make a todo which we will get from action 
+            const todo = {
+            // Now in an array here, we are pushing a todo object, as each todo is an object
+
+            // id: Date.now() 
+            // Earlier we did this for id in context API. Now we will do something else i.e. use nanoid
+                id: nanoid(), 
+                text: action.payload
+                // Here inside action, payload is an object from which we will extract texts, like we did this here
+
+                // Now we have made a todo, now to update the state
+
+            }
+            // Look at initial state, there we have a default todo, so we will access that state, and push our new todo to it
+            state.todos.push(todo)
         
+        },
+
         
+        removeTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+            // overriding these todos by adding a filter 
+            // Here todo is an individual todo inside todos, we compare here if the id of an individual todo matches with the action payload, if it does not then pass them, if it does then filter it out and delete them
+
+
+        },
+
+        updateTodo: (state, action) => {
+            // to update a new todo we will first create an object of action.payload having the todo id and a new updated text called new text
+            // We will now map this todos, using map function, if we find a todo whose id matches with the one to update, we will update it, else we pass the todos as they are
+            const {id, newText} = action.payload
+            const updatedTodo = state.todos.map((todo) => todo.id === id ? {...todo, text: newText} : todo);
+
+            state.todos = updatedTodo;
+        }
+
+
     }
 
 })
+
+// With this we finished exporting the first part, to export the second part, we do the following below
+
+// exporting the functionalities first, because we will update the state always via these reducers
+// So these individual functions will be very useful to us
+
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions
+
+// Also now we send these functionalities / reducers to the store to make the store aware of these reducers. The store will register these reducers and will update itself.
+// Hence we export the list of reducers to store
+
+export default todoSlice.reducer
+// 4. go back to store.js now
+
